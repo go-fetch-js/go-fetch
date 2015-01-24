5f1daed32d5b74058452fbacb003cdd43b4cc2de
James Newell <james@digitaledgeit.com.au>
2015-01-25 01:33:35 +1100

    updated Url.getQuery methods to get it owrking

--- test/Url.js
+++ test/Url.js
@@ -15,6 +15,46 @@ describe('Url', function() {
 
 	});
 
+	describe('.getQuery()', function() {
+
+		it('should return a dictionary when I don\'t pass a parameter name', function() {
+			var url = new Url('http://www.google.com?page=first');
+			assert.deepEqual(url.getQuery(), {page: 'first'});
+		});
+
+		it('should return a value when I pass a parameter name', function() {
+			var url = new Url('http://localhost/?letters=abc');
+			assert.equal(url.getQuery('letters'), 'abc');
+		});
+
+		it('should return undefined when the query parameter isn\'t found', function() {
+			var url = new Url('http://localhost/?letters=abc');
+			assert.equal(url.getQuery('foobar'), undefined);
+		});
+
+		it('should return the default value when the query parameter isn\'t found', function() {
+			var url = new Url('http://localhost/?letters');
+			assert.equal(url.getQuery('letters', '123'), '123');
+		});
+
+	});
+
+	describe('.getQuery()', function() {
+
+		it('should replace the dictionary when I don\'t pass a parameter name', function() {
+			var url = new Url('http://localhost/?letters=abc');
+			url.setQuery({'foobar': 'power'});
+			assert.deepEqual(url.getQuery(), {'foobar': 'power'});
+		});
+
+		it('should replace the param when I pass a parameter name and value', function() {
+			var url = new Url('http://localhost/?letters=abc');
+			url.setQuery('letters', '123');
+			assert.equal(url.getQuery('letters'), 123);
+		});
+
+	});
+
 	describe('.toString()', function() {
 
 		it('should convert the URL to a string representation of the URL', function() {
